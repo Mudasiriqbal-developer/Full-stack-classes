56363036744d854c65506c216bf0369e2a63b5b8
M Mudasir Iqbal <mr.mudasiriqbal.developer@gmail.com>
2026-03-04 19:31:39 +0500

    Add operators.js file and include script in index.html

---
+++ Code/javascript/operators.js
@@ -0,0 +1,35 @@
+const linkin = {
+    userImage: "../portfolio/images/my_pic_white-removebg-preview.png",
+    name: "Mudasir",
+    age: "21",
+    isMale: "true",
+    ischeck: "😊",
+    semister: "3rd",
+    work: "Senior Web developer",
+    detail: "I'm full stack developer",
+    projects: ["Chatbot", "eccommerce", "full stack todo app"],
+    connection: "184 connections",
+    messege: "Messages",
+
+}
+
+console.log(linkin);
+
+document.writeln(
+    "Name : ",
+    linkin.name,
+    "<br> Age: ",
+    linkin.age,
+    linkin.isMale,
+
+)
+
+
+// const num1 = prompt("Enter First Number:");
+// const num2 = prompt("Enter Second Number:");
+// const operator = prompt("Enter Operator:" );
+
+// console.log("Addition: ", (`${Number(num1)} ${typeof operator} ${Number(num2)}`));
+// console.log("Subtraction: ", (Number(num1) - Number(num2)));
+// console.log("Multiplication: ", (Number(num1) * Number(num2)));
+// console.log("Division: ", (Number(num1) / Number(num2)));
