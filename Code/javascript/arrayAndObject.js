027e8768f7ecbef92a86b7d4b8a2dc70db0cf5f8
M Mudasir Iqbal <mr.mudasiriqbal.developer@gmail.com>
2026-03-11 11:41:20 +0500

    Add HTML and JavaScript files for array and object examples

---
+++ Code/javascript/arrayAndObject.js
@@ -0,0 +1,86 @@
+// Arrow Function.
+
+// const user = (num=10, num2) => {
+//     console.log(num + num2);
+    
+// }
+// user(12,21);
+// user(,45);
+
+// function user(name, Sname) {
+//     console.log('My full name is: ', name, Sname);
+//     // console.log('My second name is: ', Sname);
+    
+// }
+// user('Mudasir', 'Ibal');
+
+// const user1 = user('Mudasir', 'Ibal');
+// console.log(user1)
+
+// const isEven = (num) => {
+//     if(num % 2 == 0) {
+//         console.log('the given number is Even: ', num);  
+//     } else {
+//         console.log('the given number is odd: ', num);
+//     }
+// }
+// let input = prompt('enter the Number to check Even or Odd')
+// isEven(input);
+
+
+// Object
+
+// let student = {
+//     name : "Mudasir",
+//     age : 21,
+//     gender : 'Male'
+// }
+// student.name = 'Mudasir iqbal';
+// console.log(student);
+
+
+// Array
+
+// let arr = [10,20,30,40,50];
+// console.log(arr[2]);
+
+// // push
+// arr.push(60);
+// console.log(arr);
+
+// // Pop
+// arr.pop();
+// console.log(arr);
+
+// // Unshift
+// arr.unshift(5);
+// console.log(arr);
+
+// // shift
+// arr.shift();
+// console.log(arr);
+
+
+
+// Activity
+
+// let student = {
+//     name: 'Mudasie',
+//     age: 21,
+//     marks: [89, 90, 87, 99]
+// }
+
+// function calcAverage(marks) {
+//     let sum = 0;
+//     for(let i=0; i<marks.length; i++) {
+//         sum += marks[i];
+//     }
+//     console.log(sum/marks.length);
+// }
+
+// calcAverage(student.marks);
+
+
+
+
+
