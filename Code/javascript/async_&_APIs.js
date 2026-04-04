06c22abbbb54244cf3cf5c7ef253a8f056bcd769
M Mudasir Iqbal <mr.mudasiriqbal.developer@gmail.com>
2026-04-04 16:48:00 +0500

    Replace async.html, async.css, and async.js with async_&_APIs.html, async_&_APIs.css, and async_&_APIs.js for improved API data handling and structure

--- Code/javascript/async_&_APIs.js
+++ Code/javascript/async_&_APIs.js
@@ -4,7 +4,10 @@ console.log("3");
 use_async();
 console.log("4");
 console.log("5");
+use_async2();
 
+// 10 users data from api.
+// render data from the api in a table format in the html page using .map() method.
 async function use_async() {
   let users = "https://jsonplaceholder.typicode.com/users";
   let response = await fetch(users);
@@ -37,5 +40,27 @@ async function use_async() {
   </table>
 </div>
 `;
+  });
+}
+
+// 100 posts from the api.
+// render data from the api in a table format in the html page using foreach loop.
+async function use_async2() {
+  let posts = "https://jsonplaceholder.typicode.com/posts";
+  let postsRes = await fetch(posts);
+  let postsData = await postsRes.json();
+  console.log(postsData);
+  let container2 = document.getElementById("box2");
+  
+  postsData.forEach(post => {
+    const div = document.createElement("div");
+    
+    div.innerHTML = `
+    <hr><hr>
+    <h3>${post.title}</h3>
+    <p>${post.body}</p>
+    `;
+    container2.appendChild(div); 
+    
   });
 }
