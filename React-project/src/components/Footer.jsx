16dac4e0aba3a83be4d351b8301857dd56464432
M Mudasir Iqbal <mr.mudasiriqbal.developer@gmail.com>
2026-04-13 20:07:21 +0500

    feat: add Footer component with copyright information

--- React-project/src/components/Footer.jsx
+++ React-project/src/components/Footer.jsx
@@ -2,7 +2,7 @@ export function Footer() {
   return (
     <>
       <h1 className=" underline h-10 bg-black text-amber-50 flex justify-center align-middle text-2xl">
-        @copyright 2026
+        @copyright 2026      by Mudasir iqbal
       </h1>
     </>
   );
