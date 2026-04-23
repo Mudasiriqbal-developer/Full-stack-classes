8973da0d1c18d015a70d9ba38faf3d35e50dc39f
M Mudasir Iqbal <mr.mudasiriqbal.developer@gmail.com>
2026-04-23 16:43:05 +0500

    feat: add Express server setup with initial route and nodemon for development

--- Backend/backend-with-express/server.js
+++ Backend/backend-with-express/server.js
@@ -1,1 +1,11 @@
-const express = 
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+app.get('/', (request, response) => {
+    response.send('Your request is processed on the...!');
+})
+
+app.listen(PORT, () => {
+    console.log("Express Server Is Running...!"); 
+})
