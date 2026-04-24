1cefb84f198eb0bcff8979fd651641b21cf372c5
M Mudasir Iqbal <mr.mudasiriqbal.developer@gmail.com>
2026-04-24 11:15:00 +0500

    feat: implement file system operations to create and delete an HTML file

--- Backend/backend-with-express/server.js
+++ Backend/backend-with-express/server.js
@@ -1,11 +1,34 @@
+const { log } = require('console');
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const PORT = 5000;
 
-app.get('/', (request, response) => {
-    response.send('Your request is processed on the...!');
-})
+// app.get('/', (request, response) => {
+//     response.send('Your request is processed on the...!');
+// })
+
+// File system
+// fs.writeFileSync('text.txt','this is very nice backend develped...');
+// // read file utf-8
+// const content = fs.readFileSync('text.txt','utf-8');
+// console.log(content);
+
+// // delete file
+// setTimeout(() => {
+//     fs.unlinkSync('text.txt')
+// }, 3000);
+
+fs.writeFileSync('index.html', '<p>this is html file.</p>')
+const data = fs.readFileSync('index.html', 'utf-8')
+console.log(data)
+setTimeout(() => {
+    fs.unlinkSync('index.html');
+}, 3000)
+
+
 
 app.listen(PORT, () => {
     console.log("Express Server Is Running...!"); 
 })
+
