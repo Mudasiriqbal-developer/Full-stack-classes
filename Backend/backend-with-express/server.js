123b2c6027fefa02d50dc88a820cefde7d872f86
M Mudasir Iqbal <mr.mudasiriqbal.developer@gmail.com>
2026-05-02 12:04:11 +0500

    feat: add bcrypt for password hashing and implement user creation and login endpoints

--- Backend/backend-with-express/server.js
+++ Backend/backend-with-express/server.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import db from './config/database.js'
 import fs from 'fs';
 import { log } from 'console';
+import bcrypt from 'bcrypt';
 const PORT = 6166;
 const app = express();
 
@@ -12,7 +13,13 @@ app.use(express.json());
 
 app.post('/create-user', async (req, res) => {
     try {
-        const newUser = await User.create(req.body)
+        
+        const hashpassword = bcrypt.hashSync(req.body.password, 10);
+        const newUser = await User.create({
+            name:req.body.name,
+            email:req.body.email,
+            password:hashpassword
+        })
         console.log(newUser)
         if(!newUser) {
             console.log("Error Occure")
@@ -23,6 +30,27 @@ app.post('/create-user', async (req, res) => {
     }
 })
 
+
+app.post('/hash-word', (req, res) => {
+    console.log(req.body.password)
+    const hash = bcrypt.hashSync(req.body.password, 10);
+    console.log(hash)
+    console.log(bcrypt.compareSync(req.body.password, hash));
+});
+
+app.post('/login',async (req,res)=>{
+
+    const find_user = await User.findOne({name:req.body.name});
+    if(!find_user) res.status(404).json({message:"User Not Found"})
+    // console.log(find_user)
+
+    if(find_user.password !== req.body.password){
+        res.status(402).json({message:"Password is Not Valid"})
+        return;
+    }
+
+res.status(200).json({find_user})
+})
 
 db();
 // app.get('/', (request, response) => {
